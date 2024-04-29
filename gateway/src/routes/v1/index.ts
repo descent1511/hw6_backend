569bfa917fad965e3ba5fa1b89c803a65ff021e8
descent1511 <natibatian@gmail.com>
2024-04-29 23:56:30 +0300

    fix: add location in route gateway

--- gateway/src/routes/v1/index.ts
+++ gateway/src/routes/v1/index.ts
@@ -5,6 +5,6 @@ const router: express.Router = express.Router();
 
 router.use('/users', proxy('http://localhost:8001'));
 router.use('/trips', proxy('http://localhost:8002'));
-
+router.use('/locations', proxy('http://localhost:8006'));
 
 export default router;
